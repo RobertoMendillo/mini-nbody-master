64044e30290cb6673ffc4e6b7d5564de8d018c52
robertomendillo <robertom.30@hotmail.it>
2026-07-24 15:08:20 +0200

    separated script for sequential and parallel execution

---
+++ parallel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+OUTPUT_FILE="results.data"
+
+echo "Esecuzione parallela" >> OUTPUT_FILE
+echo "" >> OUTPUT_FILE
+mpicc -O0 parallelized_nbody/nbody_mpi.c ./papi/papi_helper.c -lm -I./papi/ -I. /usr/local/lib/libpapi.a -o nbody_mpi.out
+mpirun -machinefile machinefile.txt nbody_mpi.out >> OUTPUT_FILE 2>&1
--- sequential.sh
+++ sequential.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 OUTPUT_FILE="results.data"
 
-echo "Esecuzione sequenziale" >> OUTPUT_FILE
+echo "Esecuzione sequenziale" >> $OUTPUT_FILE
 echo "" >> OUTPUT_FILE
 mpicc -O0 nbody.c ./papi/papi_helper.c -lm -I./papi/ /usr/local/lib/libpapi.a -o nbody.out
-mpirun -np 1 -machinefile machinefile.txt nbody.out >> OUTPUT_FILE 2>&1
+mpirun -np 1 -machinefile machinefile.txt nbody.out >> $OUTPUT_FILE 2>&1
 
-echo "" >> OUTPUT_FILE
-echo "" >> OUTPUT_FILE
-
-echo "Esecuzione parallela" >> OUTPUT_FILE
-echo "" >> OUTPUT_FILE
-mpicc -O0 parallelized_nbody/nbody_mpi.c ./papi/papi_helper.c -lm -I./papi/ /usr/local/lib/libpapi.a -o nbody_mpi.out
-mpirun -machinefile machinefile.txt nbody_mpi.out >> OUTPUT_FILE 2>&1
+echo "" >> $OUTPUT_FILE
+echo "" >> $OUTPUT_FILE
