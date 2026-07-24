7c0e70b4dea3b94c5548199c498993e806bfcd8f
robertomendillo <robertom.30@hotmail.it>
2026-07-24 15:51:20 +0200

    added machinefile for parallel execution

--- parallel.sh
+++ parallel.sh
@@ -4,4 +4,4 @@ OUTPUT_FILE="results.data"
 echo "Esecuzione parallela" >> $OUTPUT_FILE
 echo "" >> OUTPUT_FILE
 mpicc -O0 parallelized_nbody/nbody_mpi.c ./papi/papi_helper.c -lm -I./papi/ -I. /usr/local/lib/libpapi.a -o nbody_mpi.out
-mpirun -machinefile machinefile.txt nbody_mpi.out >> $OUTPUT_FILE 2>&1
+mpirun -machinefile machinefile_p.txt nbody_mpi.out >> $OUTPUT_FILE 2>&1
