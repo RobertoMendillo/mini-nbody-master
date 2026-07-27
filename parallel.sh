1efa320712d037daf7631bbf41bf3e1d223ad62e
robertomendillo <robertom.30@hotmail.it>
2026-07-27 10:49:10 +0200

    fixed logging printf and added openMP library to compilation command

--- parallel.sh
+++ parallel.sh
@@ -3,7 +3,7 @@ OUTPUT_FILE="results_parallel.data"
 
 printf "Esecuzione parallela\n" > $OUTPUT_FILE
 
-mpicc -O0 parallelized_nbody/nbody_mpi.c ./papi/papi_helper.c -lm -I./papi/ -I. /usr/local/lib/libpapi.a -o nbody_mpi.out
+mpicc -O3 parallelized_nbody/nbody_mpi.c ./papi/papi_helper.c -lm -I./papi/ -I. -fopenmp /usr/local/lib/libpapi.a -o nbody_mpi.out
 
 printf "\n\n================ 30_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
 
@@ -29,22 +29,22 @@ mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 50000 
 
 printf "\n\n================ 100_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
 
-printf "\n50_000 corpi tcpip/ethernet\n" >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 10000 >> $OUTPUT_FILE 2>&1
+printf "\n100_000 corpi tcpip/ethernet\n" >> $OUTPUT_FILE
+mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 100000 >> $OUTPUT_FILE 2>&1
 
-printf "\n50_000 corpi tcpip/infiniband\n" >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 10000 >> $OUTPUT_FILE 2>&1
+printf "\n100_000 corpi tcpip/infiniband\n" >> $OUTPUT_FILE
+mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 100000 >> $OUTPUT_FILE 2>&1
 
-printf "\n50_000 corpi native/infiniband\n" >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 10000 >> $OUTPUT_FILE 2>&1
+printf "\n100_000 corpi native/infiniband\n" >> $OUTPUT_FILE
+mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 100000 >> $OUTPUT_FILE 2>&1
 
 printf "\n\n================ 200_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
 
-printf "\n50_000 corpi tcpip/ethernet\n" >> $OUTPUT_FILE
+printf "\n200_000 corpi tcpip/ethernet\n" >> $OUTPUT_FILE
 mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 200000 >> $OUTPUT_FILE 2>&1
 
-printf "\n50_000 corpi tcpip/infiniband\n" >> $OUTPUT_FILE
+printf "\n200_000 corpi tcpip/infiniband\n" >> $OUTPUT_FILE
 mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 200000 >> $OUTPUT_FILE 2>&1
 
-printf "\n50_000 corpi native/infiniband\n" >> $OUTPUT_FILE
+printf "\n200_000 corpi native/infiniband\n" >> $OUTPUT_FILE
 mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 200000 >> $OUTPUT_FILE 2>&1
