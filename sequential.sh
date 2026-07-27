65aafd2c23a8cfedb0516b39e281f71c33906372
robertomendillo <robertom.30@hotmail.it>
2026-07-27 11:11:27 +0200

    updated script for sequential execution

--- sequential.sh
+++ sequential.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 OUTPUT_FILE="results.data"
 
-echo "Esecuzione sequenziale" >> $OUTPUT_FILE
-echo "" >> OUTPUT_FILE
-mpicc -O0 nbody.c ./papi/papi_helper.c -lm -I./papi/ /usr/local/lib/libpapi.a -o nbody.out
+printf "Esecuzione sequenziale\n" > $OUTPUT_FILE
+
+mpicc -O3 nbody.c ./papi/papi_helper.c -lm -I./papi/ /usr/local/lib/libpapi.a -o nbody.out
+
+printf "\n\n================ 30_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
 mpirun -np 1 -machinefile machinefile.txt nbody.out >> $OUTPUT_FILE 2>&1
 
-echo "" >> $OUTPUT_FILE
-echo "" >> $OUTPUT_FILE
+printf "\n\n================ 50_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
+mpirun -np 1 -machinefile machinefile.txt nbody.out 50000 >> $OUTPUT_FILE 2>&1
+
+printf "\n\n================ 100_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
+mpirun -np 1 -machinefile machinefile.txt nbody.out 100000 >> $OUTPUT_FILE 2>&1
+
+printf "\n\n================ 200_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
+mpirun -np 1 -machinefile machinefile.txt nbody.out 200000 >> $OUTPUT_FILE 2>&1
