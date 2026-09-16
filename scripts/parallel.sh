9ecc92aab61c08cb6616d243801144c89c2d3abe
Roberto Mendillo <robertom.30@hotmail.it>
2026-09-16 16:10:35 +0200

    updated script: used for loop

--- scripts/parallel.sh
+++ scripts/parallel.sh
@@ -1,95 +1,32 @@
 #!/bin/bash
 OUTPUT_FILE="results_parallel.data"
+MACHINEFILE="machinefile_p.txt"
 
-# printf "Esecuzione parallela\n" > $OUTPUT_FILE
-printf "network, processors, bodies, time\n" > $OUTPUT_FILE
+# Inizializza il file CSV
+printf "network, processors, bodies, cpu_time, net_time, total_time\n" > $OUTPUT_FILE
 
+# Compilazione
+echo "Compilazione in corso..."
 mpicc -O3 parallelized_nbody/nbody_mpi.c ./papi/papi_helper.c -lm -I./papi/ -I. -fopenmp /usr/local/lib/libpapi.a -o nbody_mpi.out
+echo "Compilazione completata. Inizio benchmark."
 
-# printf "\n\n================ 30_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
+# Array con tutte le dimensioni della simulazione
+BODIES=(30000 50000 100000 200000 400000 600000 800000 1000000)
 
-printf "tcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out >> $OUTPUT_FILE 2>&1
+for size in "${BODIES[@]}"; do
+    echo "Esecuzione simulazione per $size corpi..."
 
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out >> $OUTPUT_FILE 2>&1
+    # TCP/IP Ethernet
+    printf "tcpip/ethernet, " >> $OUTPUT_FILE
+    mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile $MACHINEFILE nbody_mpi.out "$size" >> $OUTPUT_FILE 2>&1
 
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out >> $OUTPUT_FILE 2>&1
+    # TCP/IP InfiniBand
+    printf "tcpip/infiniband, " >> $OUTPUT_FILE
+    mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile $MACHINEFILE nbody_mpi.out "$size" >> $OUTPUT_FILE 2>&1
 
-# printf "\n\n================ 50_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
+    # Native InfiniBand
+    printf "native/infiniband, " >> $OUTPUT_FILE
+    mpirun --mca btl self,openib -machinefile $MACHINEFILE nbody_mpi.out "$size" >> $OUTPUT_FILE 2>&1
+done
 
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 50000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 50000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 50000 >> $OUTPUT_FILE 2>&1
-
-# printf "\n\n================ 100_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 100000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 100000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 100000 >> $OUTPUT_FILE 2>&1
-
-# printf "\n\n================ 200_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 200000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 200000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 200000 >> $OUTPUT_FILE 2>&1
-
-# printf "\n\n================ 400_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 400000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 400000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 400000 >> $OUTPUT_FILE 2>&1
-
-# printf "\n\n================ 600_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 600000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 600000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 600000 >> $OUTPUT_FILE 2>&1
-
-# printf "\n\n================ 800_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 800000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 800000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 800000 >> $OUTPUT_FILE 2>&1
-
-# printf "\n\n================ 1_000_0000 ================\n\n" >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/ethernet, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include em2 -machinefile machinefile_p.txt nbody_mpi.out 1000000 >> $OUTPUT_FILE 2>&1
-
-printf "\ntcpip/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,tcp --mca btl_tcp_if_include ib0 -machinefile machinefile_p.txt nbody_mpi.out 1000000 >> $OUTPUT_FILE 2>&1
-
-printf "\nnative/infiniband, " >> $OUTPUT_FILE
-mpirun --mca btl self,openib -machinefile machinefile_p.txt nbody_mpi.out 1000000 >> $OUTPUT_FILE 2>&1
+echo "Benchmark completato! Risultati salvati in $OUTPUT_FILE"
