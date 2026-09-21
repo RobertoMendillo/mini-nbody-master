a2ee4681fdba2dac867c542fca38b3be77348ca4
robertomendillo <robertom.30@hotmail.it>
2026-09-21 13:14:20 +0200

    fixed size in mpirun

--- scripts/sequential.sh
+++ scripts/sequential.sh
@@ -15,7 +15,7 @@ BODIES=(30000 50000 100000 200000 400000 600000)
 
 for size in "${BODIES[@]}"; do
   echo "Esecuzione simulazione per $size corpi..."
-  mpirun -np 1 -machinefile $MACHINEFILE nbody.out 400000 >> $OUTPUT_FILE 2>&1
+mpirun -np 1 -machinefile $MACHINEFILE nbody.out "$size" >> $OUTPUT_FILE 2>&1
 done
 
 echo "Benchmark completato! Risultati salvati in $OUTPUT_FILE"
